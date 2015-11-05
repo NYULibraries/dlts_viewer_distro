12341ec7248429f81ef08af7c2fb3bd0c8a4f566
Alberto Ortiz Flores <aortiz@nyu.edu>
2015-11-05 10:21:36 -0500

    openlayers

--- bin/build_openlayers.sh
+++ bin/build_openlayers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 die () {
-  echo "file: ${0} | line: ${1} | step: ${2} | message: ${3}";
+  echo ${0};
   exit 1;
 }
 
@@ -14,12 +14,11 @@ while getopts ":b:h" opt; do
       ;;
     h)
       echo " "
-      echo " Usage: ./bin/build_openlayers.sh -c example.conf"
+      echo " Usage: ./bin/build_openlayers.sh -b /www/site/example"
       echo " "
       echo " Options:"
       echo "   -h           Show brief help"
       echo "   -b           build realpath"
-      echo "   -c <file>    Specify the configuration file to use (e.g., -c example.conf)."
       echo " "
       exit 0
       ;;
@@ -28,36 +27,26 @@ done
 
 [ -d $BUILD ] || die "Build directory ${BUILD} does not exist."
 
-if [ -d $BUILD/sites/all/modules/dlts_image ] && [ -d $BUILD/sites/all/libraries/openlayers ]
+JS_PATH=${BUILD}/sites/all/modules/dlts_viewer/js/openlayers
+
+LIB_PATH=${BUILD}/sites/all/libraries/openlayers
+
+if [ -d ${JS_PATH} ] && [ -d ${LIB_PATH} ]
   then 
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/books.cfg $BUILD/sites/all/libraries/openlayers/build/books.cfg
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTS.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/DLTS.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTSZoomIn.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Control/DLTSZoomIn.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTSZoomOut.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Control/DLTSZoomOut.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTSZoomPanel.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Control/DLTSZoomPanel.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTSZoomOutPanel.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Control/DLTSZoomOutPanel.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTSZoomInPanel.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Control/DLTSZoomInPanel.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTSScrollWheel.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Control/DLTSScrollWheel.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/DLTSMouseWheel.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Handler/DLTSMouseWheel.js
-
-    cp $BUILD/sites/all/modules/dlts_image/js/openlayers/OpenURL.js $BUILD/sites/all/libraries/openlayers/lib/OpenLayers/Layer/OpenURL.js
-
-    cd $BUILD/sites/all/libraries/openlayers/build
-
-    echo Building OpenLayers inside ${BUILD}
-    
+    cp ${JS_PATH}/books.cfg ${LIB_PATH}/build/books.cfg
+    cp ${JS_PATH}/DLTS.js ${LIB_PATH}/lib/OpenLayers/DLTS.js
+    cp ${JS_PATH}/DLTSZoomIn.js ${LIB_PATH}/lib/OpenLayers/Control/DLTSZoomIn.js
+    cp ${JS_PATH}/DLTSZoomOut.js ${LIB_PATH}/lib/OpenLayers/Control/DLTSZoomOut.js
+    cp ${JS_PATH}/DLTSZoomPanel.js ${LIB_PATH}/lib/OpenLayers/Control/DLTSZoomPanel.js
+    cp ${JS_PATH}/DLTSZoomOutPanel.js ${LIB_PATH}/lib/OpenLayers/Control/DLTSZoomOutPanel.js
+    cp ${JS_PATH}/DLTSZoomInPanel.js ${LIB_PATH}/lib/OpenLayers/Control/DLTSZoomInPanel.js
+    cp ${JS_PATH}/DLTSScrollWheel.js ${LIB_PATH}/lib/OpenLayers/Control/DLTSScrollWheel.js
+    cp ${JS_PATH}/DLTSMouseWheel.js ${LIB_PATH}/lib/OpenLayers/Handler/DLTSMouseWheel.js
+    cp ${JS_PATH}/OpenURL.js ${LIB_PATH}/lib/OpenLayers/Layer/OpenURL.js
+    cd ${LIB_PATH}/build
+    echo "Building OpenLayers inside ${BUILD}"
     ./build.py -c none books.cfg
-    
-    # go back to the directory the script was ran
     cd -
-    
 fi
+
+exit 0
